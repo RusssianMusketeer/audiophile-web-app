e16f14210f19e43f0d3684e03c9781479dd22999
RusssianMusketeer <a.devetriev@gmail.com>
2024-09-12 22:20:12 -0400

    padding and center features

--- src/app/domains/products/pages/headphone-detail/headphone-detail.component.ts
+++ src/app/domains/products/pages/headphone-detail/headphone-detail.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { PromotionComponent } from '../../../shared/components/promotion/promotion.component';
 import { ShoppingNavComponent } from '../../../shared/components/shopping-nav/shopping-nav.component';
 import { ProductDetailsComponent } from '../../components/product-details/product-details.component';
+import { FeaturesComponent } from '../../components/features/features.component';
 
 
 @Component({
   selector: 'app-headphone-detail',
   standalone: true,
-  imports: [ShoppingNavComponent, PromotionComponent, ProductDetailsComponent],
+  imports: [ShoppingNavComponent, PromotionComponent, ProductDetailsComponent,FeaturesComponent],
   templateUrl: './headphone-detail.component.html',
   styleUrl: './headphone-detail.component.css'
 })
