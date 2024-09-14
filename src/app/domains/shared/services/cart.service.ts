d350e51c6c059dac1225689d733873c74a77c9ae
RusssianMusketeer <a.devetriev@gmail.com>
2024-09-13 23:33:33 -0400

    service change

--- src/app/domains/shared/services/cart.service.ts
+++ src/app/domains/shared/services/cart.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Product } from '../models/products.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  cart = signal<Product[]>([]);
 
   constructor() { }
 }
